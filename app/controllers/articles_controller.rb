023a6c760abca8dba8ce7453d03c21f4f1823299
Jimmy Riccelli <jrriccelli@gmail.com>
2017-03-06 00:52:31 -0500

    added form partials and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,13 +3,20 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def new
     @article = Article.new
   end
 
   def edit
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id]).delete
+    flash[:notice] = "Article has been destroyed"
+    redirect_to articles_path
+
   end
 
   def create
