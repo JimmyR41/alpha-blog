fd151490416b5341b980bcd62b947e18b1cad23a
Jimmy Riccelli <jrriccelli@gmail.com>
2017-03-15 23:42:19 -0400

    added user articles view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController <ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def create
@@ -18,19 +19,25 @@ class UsersController <ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Updated succesfully"
       redirect_to articles_path
     else
       route_to "edit"
     end
+  end
+
+  def show
 
   end
 
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
